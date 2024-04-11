d910d50a4a5b223abe49f138e7ce9e9851de1f90
ferrumrt <nbveh211001@gmail.com>
2024-04-11 17:27:02 +0500

    home and login (start)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ["./core/**/*.{html,js}"],
-  theme: {
-    extend: {},
-    colors: {
-      'black-main': "#4F4F4F",
-      'accent-red': "#BA2025"
+    content: [
+        "./core/**/*.{html,js}",
+        "./users/**/*.{html,js}"
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            'black-main': "#4F4F4F",
+            'accent-red': "#BA2025",
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            emerald: colors.emerald,
+            indigo: colors.indigo,
+            yellow: colors.yellow,
+        },
+        fontFamily: {
+            "Qanelas": ['Qanelas', ...defaultTheme.fontFamily.sans],
+        }
     },
-    fontFamily: {
-      "Qanelas": ['Qanelas', ...defaultTheme.fontFamily.sans],
-    }
-  },
-  plugins: [],
+    plugins: [],
 }
 
