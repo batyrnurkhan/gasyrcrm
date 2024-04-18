d2c813c56aef4fcd823aaba8b92f0cf56265f533
Timur <nbveh211001@gmail.com>
2024-04-19 02:13:07 +0500

    Changes

--- courses/templatetags/course_tags.py
+++ courses/templatetags/course_tags.py
@@ -4,3 +4,7 @@ register = template.Library()
 @register.filter
 def is_user_enrolled(course, user):
     return course.users.filter(id=user.id).exists()
+
+@register.filter
+def format_phone_number(number):
+    return f"{number[0:2]} ({number[2:5]}) {number[5:8]} {number[8:10]} {number[10:12]}"
