678e432848b985bf39eee9b72aae187847d30393
batyrnurkhan <makyzhanbatyr@gmail.com>
2024-05-03 11:19:59 +0500

    fix creating test answers and questions

--- core/urls.py
+++ core/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path('completed-courses/', CompletedCoursesPageView.as_view(), name='completed-courses'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
