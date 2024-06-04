3b3cc7a50624335ead27aa72d2fa44c249470a66
batyrnurkhan <makyzhanbatyr@gmail.com>
2024-06-04 09:39:28 +0500

    Добавил логику чтобы психолог не мог поставить пересекающие времена

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,10 +1,27 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import Appointment
+
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = ['date', 'start_time', 'end_time', 'link', 'is_booked', 'user']
+
+    def validate(self, data):
+        appointment = Appointment(**data)
+
+        if 'user' not in data:
+            raise serializers.ValidationError("User must be specified.")
+        appointment.user = self.context['request'].user
+
+        try:
+            appointment.clean()
+        except ValidationError as e:
+            raise serializers.ValidationError({'non_field_errors': e.messages})
+
+        return data
 
 class AppointmentLinkSerializer(serializers.ModelSerializer):
     class Meta:
