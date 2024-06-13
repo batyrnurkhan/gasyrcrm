98fb531a8aa6c9a6a326da2178f3bc185bf1b5da
batyrnurkhan <makyzhanbatyr@gmail.com>
2024-06-13 15:04:00 +0500

    news

--- news/models.py
+++ news/models.py
@@ -6,8 +6,6 @@ from ckeditor_uploader.fields import RichTextUploadingField
 class News(models.Model):
     title = models.CharField(max_length=255)
     content = RichTextUploadingField()
-    quote = models.TextField(blank=True, null=True)
-    advertisement = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='news_images/', null=True, blank=True)
